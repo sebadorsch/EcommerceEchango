4511717ef495e5ea98ac6df77b3af4e0ae6f6aa8
sebastian <sdorsch@lacapital.com.ar>
2021-12-07 12:31:19 -0300

    asdasdas

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include('Echango.urls')),
     path('usuarios/', include('django.contrib.auth.urls')),
     path('usuarios/', include('usuarios.urls')),
+    path('api/', include('api.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
