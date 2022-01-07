f88986368621db502be4f5c353027cc46509d9f8
sebastian <sdorsch@lacapital.com.ar>
2022-01-07 12:18:42 -0300

    Api serializada

--- api/urls.py
+++ api/urls.py
@@ -4,12 +4,9 @@ from rest_framework.routers import DefaultRouter
 from api.views import ProductoList, ComentarioList, ProductoTalleList, UserCreate, \
     UserList, UserProfileViewSet, UserLoginApiView
 
-import rest_framework
-
-
 
 router = DefaultRouter()
-router.register('user', UserProfileViewSet, basename='user')
+router.register('users', UserProfileViewSet, basename='user')
 
 urlpatterns = [
     path(r'comentarios/', ComentarioList.as_view(), name="get_comentario"),
@@ -17,8 +14,8 @@ urlpatterns = [
     path("productos/<int:pk>/producto-talle/", ProductoTalleList.as_view(), name="get_producto_talle"),
     path(r'productos/', ProductoList.as_view(), name="get_producto"),
 
-    path(r'user/post', UserCreate.as_view(), name="user_post"),
-    path(r'user/get', UserList.as_view(), name="user_get"),
+    path(r'users/get', UserList.as_view(), name="user_get"),
+    path(r'users/post', UserCreate.as_view(), name="user_post"),
     path(r'', include(router.urls)),
 
     path("login/", UserLoginApiView.as_view(), name="api_login"),
