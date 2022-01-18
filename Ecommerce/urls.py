dbea129d479a121e2fafe0f1c9d42e92311bfb5d
sebastian <sdorsch@lacapital.com.ar>
2022-01-18 11:40:04 -0300

    Testing

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('Echango.urls')),
+
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('usuarios/', include('django.contrib.auth.urls')),
     path('usuarios/', include('usuarios.urls')),
-    path('api/', include('api.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
